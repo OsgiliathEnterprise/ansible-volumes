34b0f889d669c4d10e30b6c4b306921d671f07d4
Charlie Mordant <cmordant1@gmail.com>
2021-08-26 00:02:22 +0200

    [skip travis] update to handle latest ansible release

--- molecule/nfs/tests/test_consumer.py
+++ molecule/nfs/tests/test_consumer.py
@@ -3,7 +3,7 @@ testinfra_hosts = ["consumer.osgiliath.test"]
 
 def test_nfs_is_mounted(host):
     with host.sudo():
-        command = r"""df -h | \
-        egrep -c 'datastore\.osgiliath\.test:/var/nfs.*/net'"""
+        command = r"""mount | \
+        grep -c 'auto.consumer.osgiliath.test.net on /net type autofs'"""
         cmd = host.run(command)
     assert '1' in cmd.stdout
