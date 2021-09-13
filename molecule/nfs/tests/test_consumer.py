e81d7d8d8e41879a326913b7cbf929c9dc7851c8
Charlie Mordant <cmordant1@gmail.com>
2021-09-13 18:04:51 +0200

    configured autofs

--- molecule/nfs/tests/test_consumer.py
+++ molecule/nfs/tests/test_consumer.py
@@ -7,3 +7,11 @@ def test_nfs_is_mounted(host):
         grep -c 'auto.consumer.osgiliath.test.net on /net type autofs'"""
         cmd = host.run(command)
     assert '1' in cmd.stdout
+
+
+def test_autofs_is_activated_in_sssd(host):
+    with host.sudo():
+        command = r"""cat /etc/sssd/sssd.conf | \
+        egrep -c 'services.*=.*autofs'"""
+        cmd = host.run(command)
+    assert '1' in cmd.stdout
