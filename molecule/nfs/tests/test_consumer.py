420a5a21c1f3aad3e6f512af62762a762e97135d
Charlie Mordant <cmordant1@gmail.com>
2021-11-21 16:10:13 +0100

    set permissions on sssd.conf files

--- molecule/nfs/tests/test_consumer.py
+++ molecule/nfs/tests/test_consumer.py
@@ -1,14 +1,16 @@
 testinfra_hosts = ["consumer.osgiliath.test"]
 
-
-# def test_nfs_is_mounted(host): # TODO try to make this one working
+# TODO try to make this one working
+# def test_nfs_is_mounted(host):
 #    with host.sudo():
 #        command = r"""mount | \
 #        grep -c 'auto.consumer.osgiliath.test.net on /net type autofs'"""
 #        cmd = host.run(command)
 #    assert '1' in cmd.stdout
 
-def test_nfs_is_mounted_through_fstab(host): # TODO remove once automount is working
+
+# TODO remove once automount is working
+def test_nfs_is_mounted_through_fstab(host):
     with host.sudo():
         command = r"""mount | \
         grep -c 'datastore.osgiliath.test:/var/nfs on /net type nfs4'"""
