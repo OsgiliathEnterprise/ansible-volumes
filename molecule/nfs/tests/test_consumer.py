652d3bccf28fe958cce7640935ea0ebef7c758d5
Charlie Mordant <cmordant1@gmail.com>
2022-05-21 19:43:07 +0200

    update automount test

--- molecule/nfs/tests/test_consumer.py
+++ molecule/nfs/tests/test_consumer.py
@@ -1,26 +1,20 @@
 testinfra_hosts = ["consumer.osgiliath.test"]
 
-# TODO try to make this one working
-# def test_nfs_is_mounted(host):
-#    with host.sudo():
-#        command = r"""mount | \
-#        grep -c 'auto.consumer.osgiliath.test.net on /net type autofs'"""
-#        cmd = host.run(command)
-#    assert '1' in cmd.stdout
-
-
-# TODO remove once automount is working
-def test_nfs_is_mounted_through_fstab(host):
-    with host.sudo():
-        command = r"""mount | \
-        grep -c 'datastore.osgiliath.test:/var/nfs on /net type nfs4'"""
-        cmd = host.run(command)
-    assert '1' in cmd.stdout
-
 
 def test_autofs_is_activated_in_sssd(host):
     with host.sudo():
         command = r"""cat /etc/sssd/sssd.conf | \
         egrep -c 'services.*=.*autofs'"""
+        cmd = host.run(command)
+    assert '1' in cmd.stdout
+
+
+def test_nfs_is_mounted_through_autofs(host):
+    with host.sudo():
+        command = r"""
+        echo '123ADMin' | \
+        kinit admin > /dev/null && \
+        mount | \
+        grep -c 'auto.consumer.osgiliath.test.net on /net type autofs'"""
         cmd = host.run(command)
     assert '1' in cmd.stdout
